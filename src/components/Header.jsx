80b01ca5ac04a46a0a81e19398ba8dc7a29085ab
pablos-juan <pabloccsanchez@gmail.com>
2025-05-06 10:41:46 -0500

    Mejorar diseño del componente Header; ajustar estilos de entrada y agrupar botones de ordenamiento

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,24 +12,26 @@ export function Header ({ sort, setSort }) {
   }
 
   return (
-    <header className='flex flex-col md:flex-row justify-center items-center p-1 md:p-4 m-2 gap-2'>
+    <header className='flex flex-col md:flex-row justify-center md:items-center p-1 md:p-4 m-2 gap-2'>
       <input
         type='text'
         placeholder='TV Samsung 70°'
-        className='bg-neutral-800 text-white text-xl font-semibold h-12 w-full md:w-2/3 py-2 px-5 rounded-full placeholder:text-neutral-600 outline-offset-2 outline-amber-200/50 focus:outline-2'
+        className='bg-neutral-800 text-white text-xl font-semibold h-12 w-full md:w-3/4 py-2 px-5 rounded-full placeholder:text-neutral-600 outline-offset-2 outline-amber-200/50 focus:outline-2'
       />
 
-      <button onClick={handleClick} id={ALFABETIC_SORT} className={`${sort === ALFABETIC_SORT ? 'bg-amber-200/20 text-amber-100 hover:bg-amber-200/60 hover:text-amber-100 active:scale-150' : BUTTON_NEUTRAL_STYLE} rounded-full py-3 px-4 flex gap-2 transition-all duration-300 cursor-pointer`}>
-        <ArrowUpAZ />
-      </button>
+      <div className='flex gap-2'>
+        <button onClick={handleClick} id={ALFABETIC_SORT} className={`${sort === ALFABETIC_SORT ? 'bg-amber-200/20 text-amber-100 hover:bg-amber-200/60 hover:text-amber-100 active:scale-150' : BUTTON_NEUTRAL_STYLE} rounded-full py-3 px-4 flex gap-2 transition-all duration-300 cursor-pointer`}>
+          <ArrowUpAZ />
+        </button>
 
-      <button onClick={handleClick} id={MIN_PRICE_FIRST_SORT} className={`${sort === MIN_PRICE_FIRST_SORT ? 'bg-amber-200/20 text-amber-100 hover:bg-amber-200/60 hover:text-amber-100 active:scale-150' : BUTTON_NEUTRAL_STYLE} rounded-full py-3 px-4 flex gap-2 transition-all duration-300 cursor-pointer`}>
-        <BanknoteArrowDownIcon />
-      </button>
+        <button onClick={handleClick} id={MIN_PRICE_FIRST_SORT} className={`${sort === MIN_PRICE_FIRST_SORT ? 'bg-amber-200/20 text-amber-100 hover:bg-amber-200/60 hover:text-amber-100 active:scale-150' : BUTTON_NEUTRAL_STYLE} rounded-full py-3 px-4 flex gap-2 transition-all duration-300 cursor-pointer`}>
+          <BanknoteArrowDownIcon />
+        </button>
 
-      <button onClick={handleClick} id={MAX_PRICE_FIRST_SORT} className={`${sort === MAX_PRICE_FIRST_SORT ? 'bg-amber-200/20 text-amber-100 hover:bg-amber-200/60 hover:text-amber-100 active:scale-150' : BUTTON_NEUTRAL_STYLE} rounded-full py-3 px-4 flex gap-2 transition-all duration-300 cursor-pointer`}>
-        <BanknoteArrowUp />
-      </button>
+        <button onClick={handleClick} id={MAX_PRICE_FIRST_SORT} className={`${sort === MAX_PRICE_FIRST_SORT ? 'bg-amber-200/20 text-amber-100 hover:bg-amber-200/60 hover:text-amber-100 active:scale-150' : BUTTON_NEUTRAL_STYLE} w-fit rounded-full py-3 px-4 flex gap-2 transition-all duration-300 cursor-pointer`}>
+          <BanknoteArrowUp />
+        </button>
+      </div>
     </header>
   )
 }
