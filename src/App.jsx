626ba47aeee7657ef39a030d5d40dc0fd43190bf
pablos-juan <pabloccsanchez@gmail.com>
2025-05-26 12:43:11 -0500

    Actualizar rutas de imagen para el logo en App y Header

--- src/App.jsx
+++ src/App.jsx
@@ -38,7 +38,7 @@ export function App () {
         <div class='max-w-7xl mx-auto grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 font-semibold text-white/70'>
           <div>
             <img
-              src='public/images/ebai.webp'
+              src='/images/ebai.webp'
               alt='logo'
               className='w-28'
             />
